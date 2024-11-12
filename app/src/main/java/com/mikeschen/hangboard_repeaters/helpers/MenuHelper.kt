eec72940a98fae5e8804fcc4f60138f2dcd4a25a
Mike Schennum <mikeschenphoto@gmail.com>
2024-11-12 11:23:33 -0700

    add in instructions page

--- app/src/main/java/com/mikeschen/hangboard_repeaters/helpers/MenuHelper.kt
+++ app/src/main/java/com/mikeschen/hangboard_repeaters/helpers/MenuHelper.kt
@@ -1,11 +1,13 @@
 package com.mikeschen.hangboard_repeaters.helpers
 
 import android.content.Context
+import android.content.Intent
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 import com.mikeschen.hangboard_repeaters.R
+import com.mikeschen.hangboard_repeaters.InstructionActivity
 
 class MenuHelper(private val context: Context) {
 
@@ -17,24 +19,20 @@ class MenuHelper(private val context: Context) {
     fun handleOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_instruction -> {
-                // Uncomment and replace with actual intent if needed
-                // val instructionIntent = Intent(context, InstructionActivity::class.java)
-                // context.startActivity(instructionIntent)
-                Toast.makeText(context, "Instructions selected", Toast.LENGTH_SHORT).show()
+                 val instructionIntent = Intent(context, InstructionActivity::class.java)
+                 context.startActivity(instructionIntent)
                 true
             }
             R.id.action_log -> {
                 // Uncomment and replace with actual intent if needed
                 // val logIntent = Intent(context, LogActivity::class.java)
                 // context.startActivity(logIntent)
-                Toast.makeText(context, "Log selected", Toast.LENGTH_SHORT).show()
                 true
             }
             R.id.action_converter -> {
                 // Uncomment and replace with actual intent if needed
                 // val converterIntent = Intent(context, ConverterActivity::class.java)
                 // context.startActivity(converterIntent)
-                Toast.makeText(context, "Converter selected", Toast.LENGTH_SHORT).show()
                 true
             }
             else -> false
