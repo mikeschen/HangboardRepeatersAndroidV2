7724f78cf28f123ca63325bfc19f45f3226937c4
Mike Schennum <mikeschenphoto@gmail.com>
2024-12-30 15:31:34 -0700

    update apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mikeschen.www.hangboardrepeaters"
         minSdk = 28
         targetSdk = 35
-        versionCode = 206
-        versionName = "206"
+        versionCode = 207
+        versionName = "207"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
